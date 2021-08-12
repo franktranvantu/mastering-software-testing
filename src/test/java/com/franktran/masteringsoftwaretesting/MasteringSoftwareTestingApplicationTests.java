3e5fa5edb954417f5bb4561070932b2db41ab4dd
frank <franktranvantu@gmail.com>
2021-08-12 20:45:54 +0700

    mastering-software-testing-1 Cloning Repo and Getting Started

---
+++ src/test/java/com/franktran/masteringsoftwaretesting/MasteringSoftwareTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franktran.masteringsoftwaretesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasteringSoftwareTestingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
