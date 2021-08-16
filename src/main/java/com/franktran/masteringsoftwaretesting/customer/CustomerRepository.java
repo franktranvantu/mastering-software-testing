481899f77ad19d859ae1c350be1fe70ffc1ff6e1
frank <franktranvantu@gmail.com>
2021-08-16 21:46:07 +0700

    mastering-software-testing-8 JPA Custom Queries

--- src/main/java/com/franktran/masteringsoftwaretesting/customer/CustomerRepository.java
+++ src/main/java/com/franktran/masteringsoftwaretesting/customer/CustomerRepository.java
@@ -1,8 +1,19 @@
 package com.franktran.masteringsoftwaretesting.customer;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CustomerRepository extends CrudRepository<Customer, UUID> {
+
+  @Query(value =
+      "SELECT id, name, phone_number " +
+      "FROM customer " +
+      "WHERE phone_number = :phone_number",
+      nativeQuery = true
+  )
+  Optional<Customer> findCustomerByPhoneNumber(@Param("phone_number") String phoneNumber);
 }
